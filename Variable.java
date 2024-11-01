c9d6d229a24dac17f27cdd248385255bfcced1b0
Anushka Patkar <144523727+anushka072005@users.noreply.github.com>
2024-11-02 00:31:39 +0545

    Add files via upload

---
+++ Variable.java
@@ -0,0 +1,15 @@
+public class Variable {
+    public static void main(String args[]) {
+        int a = 100;
+        int b = 50;
+        System.out.println(a);
+        System.out.println(b);
+        String name = "Krishna";
+        System.out.println(name);
+
+        a = 20;  // can be modify val of a 
+        System.out.println(a);
+        a = b;
+        System.out.println(a);  // Assigned val of b to the a
+    }
+}
