afc1b60503e95431c7138b2a8ded0c64b5c8c089
Anushka Patkar <144523727+anushka072005@users.noreply.github.com>
2024-11-02 00:30:29 +0545

    Add files via upload

---
+++ JavaBasic.java
@@ -0,0 +1,10 @@
+public class JavaBasic {
+    public static void main(String args[]) {
+        System.out.print("Hello World!"); 
+        System.out.println("Hello Anushka");
+        System.out.println("Hello Kaju");
+        System.out.print("Hello Kaju\n");
+        System.out.print("Hello Kaju\n");
+        System.out.print("Hello World! \nHello \n World ");
+    }
+}
