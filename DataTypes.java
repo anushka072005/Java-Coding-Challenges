75ce1aaef9f44359135fb1d3f8acee33bc61b745
Anushka Patkar <144523727+anushka072005@users.noreply.github.com>
2024-11-02 00:30:54 +0545

    Add files via upload

---
+++ DataTypes.java
@@ -0,0 +1,28 @@
+public class DataTypes {
+    public static void main(String args[]) {
+        byte b = 8;
+        System.out.println(b);
+        int i = 5;
+        System.out.println(i);
+        boolean bool = false;
+        System.out.println(bool);
+        float f = 31.3f;
+        System.out.println(f);
+        double d = 738468.309847;
+        System.out.println(d);
+        long l = 464876487;
+        System.out.println(l);
+        short n = 473;
+        System.out.println(n);
+        char ch = 'a';
+        System.out.println(ch);
+
+        int x = 10;
+        int y = 100;
+        int sum = x + y;
+        System.out.println(sum);
+
+
+    }
+    
+}
