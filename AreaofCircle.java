14351c3bf078357a382cc0fa93a7f6c6a2454a08
Anushka Patkar <144523727+anushka072005@users.noreply.github.com>
2024-11-02 16:10:02 +0545

    Add files via upload

---
+++ AreaofCircle.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class AreaofCircle {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        float pi = 3.14f;
+        float r = sc.nextFloat();
+        float areaOfCircle = pi * r * r;
+        System.out.println(areaOfCircle);
+
+        sc.close();
+    }
+}
