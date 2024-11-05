9a5b587bc7111a2f54a9eca4e11c69607a6b9c95
Anushka Patkar <144523727+anushka072005@users.noreply.github.com>
2024-11-05 14:59:22 +0545

    Add files via upload

---
+++ UnaryOperator.java
@@ -0,0 +1,25 @@
+public class UnaryOperator {
+    public static void main(String[] args) {
+
+        // int a = 10 ;
+        // int b = a++;
+        // System.out.println(a);
+        // System.out.println(b);  // first use val of (a) to create (b) then change to a 
+
+        // int a = 10 ;
+        // int b = a++;
+        // System.out.println(a);
+        // System.out.println(b);   // first change (a) then use to craete (b)
+
+        // int a = 10 ;
+        // int b = a--;
+        // System.out.println(a);
+        // System.out.println(b); 
+
+        int a = 10 ;
+        int b = ++a ;
+        System.out.println(a);
+        System.out.println(b); 
+    }
+    
+}
