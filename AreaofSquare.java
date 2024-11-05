decebad06413173cb2f01486c387a3894b473802
Anushka Patkar <144523727+anushka072005@users.noreply.github.com>
2024-11-05 15:01:00 +0545

    Add files via upload

---
+++ AreaofSquare.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class AreaofSquare {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        float side = sc.nextFloat();
+
+        float areaOfSquare = side* side;
+
+        System.out.println("Area of Square is : " + areaOfSquare);
+
+        sc.close();
+    }   
+}
