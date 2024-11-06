1504189ee2218ac8298fff52423e19e8dd3f11c2
Anushka Patkar <144523727+anushka072005@users.noreply.github.com>
2024-11-06 19:29:25 +0530

    Add files via upload

---
+++ FindLargestOf2Num.java
@@ -0,0 +1,13 @@
+public class FindLargestOf2Num {
+    public static void main(String args[]){
+        int A = 2;
+        int B = 3;
+
+        if (A>=B){
+            System.out.println("A is is Largest " + A);
+        }
+        else{
+            System.out.println("B is is Largest " + B);
+        }
+    }
+}
