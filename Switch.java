302e6a78b1e506f4dcb7945b2d015a1b3c928c4d
Anushka Patkar <144523727+anushka072005@users.noreply.github.com>
2024-11-10 23:13:57 +0530

    Add files via upload

---
+++ Switch.java
@@ -0,0 +1,18 @@
+public class Switch {
+    public static void main(String[] args) {
+        int number = 3;
+
+        switch (number) {
+            case 1: System.out.println("Pizza");
+                        break;
+            
+            case 2 : System.out.println("Momos");
+                        break;
+            
+            case 3 : System.out.println("Sweet");
+                        break;
+
+            default : System.out.println("we wake up");
+        }
+    }
+}
