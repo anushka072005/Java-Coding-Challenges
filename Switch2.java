62ee940ea2c133085a9eb8c634e93e38e6c1e41a
Anushka Patkar <144523727+anushka072005@users.noreply.github.com>
2024-11-10 23:13:20 +0530

    Add files via upload

---
+++ Switch2.java
@@ -0,0 +1,19 @@
+public class Switch2 {
+    public static void main(String args[]){
+        char ch = 'R';
+
+        switch (ch) {
+            case 'A': System.out.println("Anushka");
+                break;
+
+            case 'B' : System.out.println("Bhumi");
+                break;
+
+            case 'S' : System.out.println("Shivansh");
+                break;
+
+            default: System.out.println("No One");
+                break;
+        } 
+    }   
+}
