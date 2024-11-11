3dd15ddde2ae20eebed1f0efeb1d1684c5d92c25
Anushka Patkar <144523727+anushka072005@users.noreply.github.com>
2024-11-12 01:13:33 +0530

    Add files via upload

---
+++ Do_While.java
@@ -0,0 +1,10 @@
+public class Do_While {
+    public static void main(String[] args) {
+        int counter = 1;
+        
+        do {
+            System.out.println("Darling");
+            counter++;
+        }while(counter >= 5);
+    }
+}
