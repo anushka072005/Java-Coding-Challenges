89cd4cd621233dc07dcff6cbf5ac2cd12f3be8ce
Anushka Patkar <144523727+anushka072005@users.noreply.github.com>
2024-11-12 01:12:42 +0530

    Add files via upload

---
+++ Continue.java
@@ -0,0 +1,12 @@
+public class Continue {
+    public static void main(String[] args) {
+
+        for (int i=1 ; i<=5 ; i++){
+            if (i == 3){
+                System.out.println("Skip it");
+                continue;
+            }
+            System.out.println("1-caror");
+        }
+    }
+}
