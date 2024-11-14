cc5b2e6ec4d5b98e3f7b74ef016cadec4ce21360
Anushka Patkar <144523727+anushka072005@users.noreply.github.com>
2024-11-14 19:28:47 +0530

    Add files via upload

---
+++ Function.java
@@ -0,0 +1,11 @@
+public class Function {
+
+    public static void printHelloWorld() {
+        System.out.println("Hello Anushka");
+        System.out.println("Hello Anushka");
+        System.out.println("Hello Anushka");
+    }
+    public static void main(String[] args) {
+        printHelloWorld();        
+    }
+}
