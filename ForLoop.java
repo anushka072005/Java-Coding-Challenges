1c8a15f40490f976202d27c7e123a41c0302ceb4
Anushka Patkar <144523727+anushka072005@users.noreply.github.com>
2024-11-16 21:40:49 +0530

    Add files via upload

---
+++ ForLoop.java
@@ -0,0 +1,9 @@
+public class ForLoop {
+    public static void main(String[] args) {
+
+        int n = 5;
+        for (int i=1 ; i<=n ; i++) {
+            System.out.println(i + " Hello Darling");
+        }
+    }
+}
