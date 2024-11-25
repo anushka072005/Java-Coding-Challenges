e5fa3cb42429b7dc68a49802c883fb5403f3d5e9
Anushka Patkar <144523727+anushka072005@users.noreply.github.com>
2024-11-25 22:56:48 +0530

    Add files via upload

---
+++ BinToDecimal.java
@@ -0,0 +1,23 @@
+public class BinToDecimal {
+
+    public static void BinaryToDecimal(int binNum){
+
+        int mynum = binNum;
+        int pow = 0;
+        int  dec = 0;
+
+        while (binNum > 0) {
+            int lastDigit = binNum % 10;
+            dec = dec + (lastDigit * (int)Math.pow(2, pow));
+            
+            pow++;
+            binNum = binNum / 10;
+        }
+
+        System.out.println("Decimal of " + mynum + " : " + dec);
+    }
+    
+    public static void main(String[] args) {
+        BinaryToDecimal(1111);
+    }
+}
