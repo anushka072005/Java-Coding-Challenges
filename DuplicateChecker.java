9236d6330a2b54871deb61d3c4db7f5248041046
Anushka Patkar <144523727+anushka072005@users.noreply.github.com>
2024-12-03 21:21:15 +0530

    Add files via upload

---
+++ DuplicateChecker.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class DuplicateChecker {
+
+    public static boolean TestDuplicates(int nums[]) { 
+
+        for(int i = 0; i < nums.length; i++) {
+            for(int j = i + 1; j < nums.length; j++) {
+                if (nums[i] == nums[j]) {
+                    return true; // Exit the function once a duplicate is found
+                }
+            }
+        }
+        return false;    // If no duplicates are found
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of element in array : ");
+        int n = sc.nextInt();
+
+        int nums[] = new int[n]; // create array
+
+        System.out.println("Enter array elements : ");
+        for(int i = 0; i < n; i++) {
+            nums[i] = sc.nextInt();
+        }
+
+        boolean show = TestDuplicates(nums);
+        System.out.println(show);
+
+        sc.close();
+    }
+}
