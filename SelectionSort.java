c02defb6d7ba174259f94712420c7cc54fae3d0b
Anushka Patkar <144523727+anushka072005@users.noreply.github.com>
2024-12-16 21:15:29 +0530

    Add files via upload

---
+++ SelectionSort.java
@@ -0,0 +1,31 @@
+public class SelectionSort {
+
+    public static void selectionSort(int arr[]) {
+        for(int i = 0; i < arr.length-1; i++) {
+            int minpos = i;
+            for(int j = i+1; j < arr.length; j++) {
+                if(arr[j] < arr[minpos]) {
+                    minpos = j; //smallest number
+                }
+            }
+            // Swap [i = midpos]
+            int temp = arr[minpos];
+            arr[minpos] = arr[i];
+            arr[i] = temp;
+        }
+    }
+
+    public static void printSortedarray(int arr[]) {
+        System.out.println("after sorting");
+        for(int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+
+    public static void main(String[] args) {
+        int arr[] = {8,7,2,4,1,5};
+
+        selectionSort(arr);
+        printSortedarray(arr);
+    }
+}
