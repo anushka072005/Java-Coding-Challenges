492149fb1d3401b664137851830e98675c610dde
Anushka Patkar <144523727+anushka072005@users.noreply.github.com>
2025-02-28 18:37:30 +0545

    Add files via upload

--- BubbleSort.java
+++ BubbleSort.java
@@ -2,6 +2,7 @@ public class BubbleSort {
 
     public static void bubbleSort(int arr[]) {
 
+        // i = turn
         for(int i = 0; i < arr.length-1; i++) {
             for(int j = 0; j < arr.length-1-i; j++) {
                 if(arr[j] > arr[j+1]) {
