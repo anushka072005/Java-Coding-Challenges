490e016cfa0ee0b5db52d6e30d94217ab1ffefec
Anushka Patkar <144523727+anushka072005@users.noreply.github.com>
2025-03-31 12:04:35 +0545

    Add files via upload

---
+++ AddBinary.java
@@ -0,0 +1,17 @@
+public class AddBinary {
+    public static String addBinary(String a, String b) {
+
+        //Convert String into int
+        int num1 = Integer.parseInt(a, 2);
+        int num2 = Integer.parseInt(b, 2);
+        int sum = num1 + num2;  // Add the numbers
+        return Integer.toBinaryString(sum);   // Convert the sum back to binary
+    }
+
+    public static void main(String[] args) {
+        String a = "11";
+        String b = "1";
+
+        System.out.println(addBinary(a, b));
+    }    
+}
