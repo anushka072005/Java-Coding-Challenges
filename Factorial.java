a99ccd1163ebbe65ad527b42e5c5e684563955f3
Anushka Patkar <144523727+anushka072005@users.noreply.github.com>
2025-04-07 23:45:17 +0545

    Add files via upload

---
+++ Factorial.java
@@ -0,0 +1,15 @@
+public class Factorial {
+    public static int fact(int n) {
+        int fac;
+        if(n == 0) {
+            return 1;
+        }
+
+        fac = n * fact(n-1);
+        return fac;
+    }
+    public static void main(String[] args) {
+        int n = 5;
+        System.out.println(fact(n));
+    }
+}
