d9cb5a25a018567d4bb4d3e427e596564c11afb0
Anushka Patkar <144523727+anushka072005@users.noreply.github.com>
2025-05-13 22:12:44 +0545

    Create  printList.java

---
+++ printList.java
@@ -0,0 +1,30 @@
+public class LinkedListCreation {
+    public static class Node {
+        int data;
+        Node next;
+
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+    public static Node head;
+    public static Node tail;
+    public static int size;
+
+// Time com = O(1)
+    // Print Linkedlist
+    public void printList() {
+        // when linkedlist empty
+        if(head == null) {
+            System.out.println("Linkedlist is empty");
+            return;
+        }
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + "->");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
+
