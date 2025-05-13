9c3142bf8838e902c93c166aae939eefa3a051ff
Anushka Patkar <144523727+anushka072005@users.noreply.github.com>
2025-05-13 22:25:08 +0545

    Update and rename IterativeSearch.java to  reverseLinkedlist.java

---
+++ reverseLinkedlist.java
@@ -0,0 +1,29 @@
+public class LinkedListCreation {
+    public static class Node {
+        int data;
+        Node next;
+
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+    public static Node head;
+    public static Node tail;
+    public static int size;
+
+// Reverse LL , time com = O(n) , SC = O(1)
+    public void reverseLinkedlist() {
+        Node prev = null;
+        Node curr = tail = head;
+        Node next;
+
+        while (curr != null) {
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+
+        head = prev;
+    }
