bfe66211f69f283bdd379bce74a37c676a224ad7
Anushka Patkar <144523727+anushka072005@users.noreply.github.com>
2025-05-13 22:27:24 +0545

    Create  deleteNthFromEnd.java

---
+++ deleteNthFromEnd.java
@@ -0,0 +1,29 @@
+// Time com = O(n)  , SC = O(1)
+    // Find and Remove Nth node From End
+    public void deleteNthFromEnd(int n) {
+        // Calc Size
+        int size = 0;
+        Node temp = head;
+        while (temp != null) {
+            temp = temp.next;
+            size++;
+        }
+
+        // when need to delete "head" 
+        if(n == size) {
+            head = head.next;
+            return;
+        }
+
+        // delete nth node
+        int i = 1;
+        int iToFind = size-n; // actual Nth prev node
+        Node prev = head;
+        while (i < iToFind) {
+            prev = prev.next;
+            i++;
+        }
+
+        prev.next = prev.next.next;
+        return;
+    }
