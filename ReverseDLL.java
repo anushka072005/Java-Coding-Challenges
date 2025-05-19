e4f17a0f7e3a3d8a3e54896fb96cf5423f133098
Anushka Patkar <144523727+anushka072005@users.noreply.github.com>
2025-05-19 22:05:33 +0545

    Create ReverseDLL.java

---
+++ ReverseDLL.java
@@ -0,0 +1,16 @@
+// reverse DLL
+    public void reverse() {
+        Node curr = head;
+        Node prev = null;
+        Node next;
+    
+        while(curr != null) {
+            next = curr.next;
+            curr.next = prev;
+            curr.prev = next;
+    
+            prev = curr;
+            curr = next;
+        }
+        head = prev;
+    }
