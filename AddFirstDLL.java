13b9b1efab73185164baed66a62c08d34297bae1
Anushka Patkar <144523727+anushka072005@users.noreply.github.com>
2025-05-19 22:00:00 +0545

    Create AddFirstDLL.java

---
+++ AddFirstDLL.java
@@ -0,0 +1,32 @@
+import java.util.LinkedList;
+
+public class LinkedListDoubly {
+    public class Node {
+        int data;
+        Node next;
+        Node prev;
+
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+            this.prev = null;
+        }
+    }
+
+    public static Node head;
+    public static Node tail;
+    public static int size; 
+
+    // addfirst in DLL
+    public void addfirst(int data)  {
+        Node newNode = new Node(data);
+        size++;
+        if(head == null) {
+            head = tail = newNode;
+            return;
+        }
+
+        newNode.next = head;
+        head.prev = newNode;
+        head = newNode;
+    }
