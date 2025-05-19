af76d4f3f8db32c7f5ed590de56fd62568e9a7fe
Anushka Patkar <144523727+anushka072005@users.noreply.github.com>
2025-05-19 22:06:11 +0545

    Create PrintDLL.java

---
+++ PrintDLL.java
@@ -0,0 +1,9 @@
+// print DLL
+    public void print() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " <-> ");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
