247efd6422fefe0c1e3960facbbe62dd69d79fb8
Anushka Patkar <144523727+anushka072005@users.noreply.github.com>
2025-05-19 22:00:38 +0545

    Create AddLastDLL.java

---
+++ AddLastDLL.java
@@ -0,0 +1,13 @@
+// addLast DLL
+    public void addLast(int data) {
+        Node newNode = new Node(data);
+        size++;
+        if(head == null) {
+            head = tail = newNode;
+            return;
+        }
+
+        tail.next = newNode;
+        newNode.prev = tail;
+        tail = newNode;
+    }
