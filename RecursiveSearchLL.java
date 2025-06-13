adf451c6cf9762cbe178bfde3dc4461eaeb2218a
Anushka Patkar <144523727+anushka072005@users.noreply.github.com>
2025-06-13 23:54:17 +0545

    Update RecursiveSearchLL.java

--- RecursiveSearchLL.java
+++ RecursiveSearchLL.java
@@ -2,7 +2,7 @@ public class LinkedListCreation {
     public static class Node {
         int data;
         Node next;
-  
+   
         public Node(int data) {
             this.data = data;
             this.next = null; 
