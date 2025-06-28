05a1aa26ff85946886dd14ce1a1089c906b24031
Anushka Patkar <144523727+anushka072005@users.noreply.github.com>
2025-06-28 20:00:35 +0545

    Update CharecterPettern.java

--- CharecterPettern.java
+++ CharecterPettern.java
@@ -4,7 +4,7 @@ public class CharecterPettern {
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter number for line : ");
-        int n = sc.nextInt();
+        int n = sc.nextInt(); 
         char ch = 'A';
 
         for (int i = 1 ; i <= n ; i++){
